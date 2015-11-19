ced510cef68f6e27a3bd0e391aa952bb08edf5a2
jbrooks036 <joannbrooks036@gmail.com>
2015-11-19 13:34:42 -0600

    added JitterUser model and tests

---
+++ Jitter/Models/JitterUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Jitter.Models
+{
+    public class JitterUser
+    {
+        [Key]
+        public int JitterUserId { get; set; }
+        
+        [MaxLength(161)]
+        public object Description { get; set; }
+
+        public object FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        [RegularExpression(@"^[a-z\d]+[-_a-zA-Z\d]{0,2}[a-zA-Z\d]+")]
+        public object Handle { get; set; }
+
+        public object LastName { get; set; }
+        public object Picture { get; set; }
+    }
+}
